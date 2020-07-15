7170e8c401cd184b5a213724b5eb97670cfac986
Noor M. I. Albardawil <67733645+noor2020-eng@users.noreply.github.com>
2020-07-15 23:58:54 +0300

    first commit

---
+++ template/header.php
@@ -0,0 +1,39 @@
+<head>
+  <title>Noor Fashions</title>
+  <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+    .brand{
+      background: #c51162 !important;
+    }
+    .brand-text{
+      color:#c51162 !important;
+    }
+    .input-text{
+    	color: #c51162 !important;
+    }
+    form{
+      max-width: 460px;
+      margin: 20px auto;
+      padding: 20px;
+    }
+    .shope{
+      width: 100px;
+      margin: 20px auto;
+      padding: 20px;
+      display: block;;
+      position: relative;
+      top: -30px;
+    }
+    </style>
+</head>
+<body class="black lighten-4">
+  <nav class="white z-depth-0">
+    <div class="container">
+      <a href="index.php" class="brand-logo brand-text">Noor Fashions</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li> <a href="add.php" class="btn brand z-depth-0">Add a Shope</a> </li>
+        <li>  <input class=" z-depth-0 " type="text" name="search" placeholder="Search.."></li>
+      </ul>
+    </div>
+  </nav>
